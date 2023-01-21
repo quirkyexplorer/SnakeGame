f25cb2bda9c4672abf5d0c70e77f6d43c24d8a6b
Daniel Segura <quirkyexplorer@gmail.com>
2023-01-21 16:35:31 -0500

    square is able to move about. next is animation

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, 30, 30);
 
 function drawSnake() {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.height); // clearing the canvas so it doesnt drag the square
-
-
-
-
-
      // the square
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(30, 0);
-    ctx.lineTo(30, 30);
-    ctx.lineTo(0, 30);
-    ctx.lineTo(0, 0);
+    ctx.moveTo(0 + deltaX, 0 + deltaY);
+    ctx.lineTo(20 + deltaX, 0 + deltaY);
+    ctx.lineTo(20 + deltaX, 20 + deltaY);
+    ctx.lineTo(0 + deltaX, 20 + deltaY);
+    ctx.lineTo(0 + deltaX, 0 + deltaY);
+
+    // the outline
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#000000';
+    ctx.stroke();
+
+    // the fill color
+    ctx.fillStyle = "#000000";
+    ctx.fill();
     
     ctx.closePath();
     
 }
 
+window.addEventListener("keydown", move, false); // here move is the event handler
+//The addEventListener() method allows you to add event listeners on any HTML DOM object such as HTML elements, 
+//the HTML document, the window object, or other objects that support events, 
+//like the xmlHttpRequest object.
 
-drawSnake();
 
-
-let deltaX = 0;
-let deltaY = 0;
-
-canvas.addEventListener("keydown", move, false); // here move is the event handler
+ var deltaX = 0;
+ var deltaY = 0;
 
 function move(e) {
+   
     switch(e.keyCode){
         case 37: // left key pressed
-            deltaX -= 2;
+            deltaX -= 20;
             break;
         case 38: // up arrow
-            deltaY -= 2;
+            deltaY -= 20;
             break;
         case 39: // right arrow
-            deltaX += 2;
+            deltaX += 20;
             break;
         case 40: // down arrow
-            deltaY += 2;
+            deltaY += 20;
             break;
 
     }
+
+    drawSnake();
 }
