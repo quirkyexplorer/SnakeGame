169c6b1127e77ca4e05ad8ad7411ecc2526d2529
Daniel Segura <quirkyexplorer@gmail.com>
2023-01-23 22:15:25 -0500

    some changes on the code. next gotta work on the setTime out and game over

--- index.js
+++ index.js
@@ -3,102 +3,110 @@ const ctx = canvas.getContext("2d");
 
 
 
-// this one draws the background : called in drawsnake
-function drawBackground() {
-    ctx.beginPath();
-    // color in the background
-    ctx.fillStyle = "lightgreen";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+let speed = 4;
+let tileCount = 20; // number of preferred tiles
+let tileSize = canvas.clientWidth/tileCount-1; // clientWidth returns the width of an element in pixels
+
+let headX = 300; // starting position of snake
+let headY = 300;
+
+// array for snake parts
+
+const snakeParts = [];
+let tailLength = 2;
+
+// initialize the speed of the snake
+let xvelocity = 0;
+let yvelocity = 0;
+
+// draw apple = 5;
+let appleX = 5;
+let appleY = 5;
+
+// scores 
+let score = 0;
+
+// create game loop-to continously update screen
+function drawGame() {
+    changeSnakePosition(); 
+    clearScreen();
+    drawSnake();
+    setTimeout(drawGame, 1000/speed); // update screen 4 times a second
+    gameOver();  // gota check game over from here .
+    if (gameOver == true) {
+        console.log(gameOver());  // dont update? 
+    } 
 }
 
-
-// this sets up the speed of the snake 
-let fps = 03;
-let interval = Math.floor(1000 / fps);
-let startTime = performance.now();
-let previousTime = startTime; 
-
-let currentTime = 0;
-let deltaTime = 0;
-
-
-function animationLoop(timestamp) {
-
-    currentTime = timestamp;
-    deltaTime = currentTime - previousTime;
-
-    if (deltaTime > interval) {
-        previousTime = currentTime - (deltaTime % interval);
-        drawSnake();
+function gameOver() {
+    if (headX - 20 > 600) {
+        
+        return true; // dont update. freeze the image. 
     }
 
-    requestAnimationFrame(animationLoop);
+    else return false;
+    
 }
 
-requestAnimationFrame(animationLoop);
+
+function clearScreen() {
+    ctx.fillStyle = "lightblue"; // screen is blue
+    ctx.fillRect(0,0, canvas.clientWidth, canvas.clientHeight); // this whole thing clears the screen
+}
 
 
 function drawSnake() {
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height); // clearing the canvas so it doesnt drag the square
-     
-    drawBackground();
+    ctx.fillStyle = "green";
 
-    ctx.fillStyle = "black";
-    ctx.fillRect(300 + deltaX, 300 + deltaY, 20, 20); // x, y coordinates, then width and height 
-
-
-    // the outline
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
-
-    // the fill color
-    ctx.fillStyle = "#000000";
-    ctx.fill();
-
-    //deltaX += 20; //starts moving right
-    
+    ctx.fillStyle = "orange";
+    ctx.fillRect(headX, headY, tileSize, tileSize);
 
 }
 
 
-    window.addEventListener("keydown", direction, false);
+function changeSnakePosition() {
+    headX = headX + xvelocity;
+    headY = headY + yvelocity;
+}
 
+window.addEventListener('keydown', keyDown, false);
 
-// these track the movement of the square
- var deltaX = 0;
- var deltaY = 0;
+function keyDown(e) {
 
- // in order to move the snake about 
-function direction(e) {
+    switch(e.keyCode) {
 
-   
-    switch(e.keyCode){
-        case 37: // left key pressed
-        // this should lead to square animating left
-            deltaX -= 20;
-            deltaY = 0;
-            console.log("pressing left")
-            break;
-        case 38: // up arrow
-        // this should lead to square animating up
-            deltaY -= 20;
-            deltaX = 0;
-            console.log("pressing up")
-            break;
-        case 39: // right arrow
-        // this should lead to square animating right
-            deltaX += 20;
-            deltaY = 0;
-            console.log("pressing right")
-            break;
-        case 40: // down arrow
-        // this should lead to square animating down 
-            deltaY += 20;
-            deltaX = 0;
-            console.log("pressing down")
+        case 37: //left 
+            // prevent opposite move
+            if (xvelocity == tileSize)
+            return;
+            yvelocity = 0;
+            xvelocity = -1 * tileSize;
+            //console.log("pressing left")
             break;
 
+        case 38: // up 
+            if (yvelocity == tileSize)
+            return; 
+            yvelocity = -1 * tileSize;
+            xvelocity = 0; 
+            break;
+
+        case 39: // right
+            if (xvelocity == -1 * tileSize)
+            return;
+            xvelocity = tileSize;
+            yvelocity = 0;
+            break;
+
+        case 40: // down
+            if (yvelocity == -1 * tileSize)
+            return;
+            xvelocity = 0;
+            yvelocity = tileSize;
+            break;
     }
-
 }
+
+
+drawGame();
+
