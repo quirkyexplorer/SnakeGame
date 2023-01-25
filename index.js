b9c47cdc6de3a2cfd3b86ac3b6562da574dba07b
Daniel Segura <quirkyexplorer@gmail.com>
2023-01-25 12:41:21 -0500

    created game over, plus apple animation. next is body of snake bug - once the snake hits the wall is impossible for it to move along the wall - check for the snake to have the command to move around the wall - if not such comman is found wihin a few seconds then snake stops

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
-
 let speed = 4;
 let tileCount = 20; // number of preferred tiles
-let tileSize = canvas.clientWidth/tileCount-1; // clientWidth returns the width of an element in pixels
+let tileSize = 30// clientWidth returns the width of an element in pixels
 
 let headX = 300; // starting position of snake
 let headY = 300;
+
+
+// random range for apple to appear 
+min = 0;
+max = 600;
 
 // array for snake parts
 
@@ -20,8 +23,8 @@ let xvelocity = 0;
 let yvelocity = 0;
 
 // draw apple = 5;
-let appleX = 5;
-let appleY = 5;
+let appleX = 30;
+let appleY = 30;
 
 // scores 
 let score = 0;
@@ -31,23 +34,28 @@ function drawGame() {
     changeSnakePosition(); 
     clearScreen();
     drawSnake();
-    setTimeout(drawGame, 1000/speed); // update screen 4 times a second
-    gameOver();  // gota check game over from here .
-    if (gameOver == true) {
-        console.log(gameOver());  // dont update? 
+    drawApple(); // apple has to stay, it cant be redrawn everytime. 
+                // a helper function must update it once snake eats it. 
+    const time = setTimeout(drawGame, 1000/speed); // update screen 4 times a second
+    gameOver(time);
+    // console.log(getRandomInt(min, max));
+}
+
+
+function gameOver(time) {  // this function freezes the image when 
+    // snake hits a wall 
+    if (headX + 60 > 600 || headX - 30 < 0 || headY + 60 >
+        600 || headY - 30 < 0) {
+       clearTimeout(time); // dont update if going
+       // outside the bounds. freeze the image. 
     } 
+
 }
 
-function gameOver() {
-    if (headX - 20 > 600) {
-        
-        return true; // dont update. freeze the image. 
-    }
 
-    else return false;
-    
+function getRandomInt(min, max, tilesize) {
+    return Math.floor(Math.random() * ((max - min) / tileSize)) * tileSize + min;// max is inclusive and min is inclusive
 }
-
 
 function clearScreen() {
     ctx.fillStyle = "lightblue"; // screen is blue
@@ -56,17 +64,39 @@ function clearScreen() {
 
 
 function drawSnake() {
-    ctx.fillStyle = "green";
-
+    // ctx.clearRect();
     ctx.fillStyle = "orange";
     ctx.fillRect(headX, headY, tileSize, tileSize);
-
 }
 
+
+function drawApple() {
+    
+        // this.appleX = getRandomInt(min, max, tileSize);
+        // this.appleY = getRandomInt(min, max, tileSize);
+        ctx.fillStyle = "red";
+        ctx.fillRect(appleX, appleY, tileSize, tileSize);
+}
+
+// function score_increment () {
+
+
+//     if (headX == appleX && headY == appleY) {
+        
+//     };
+
+// }
 
 function changeSnakePosition() {
     headX = headX + xvelocity;
     headY = headY + yvelocity;
+    console.log("headX", headX, "headY", headY);
+    if (headX == appleX && headY == appleY) {
+        console.log("hit");
+        appleX = getRandomInt(min, max, tileSize);
+        appleY = getRandomInt(min, max, tileSize);
+        console.log("appleX", appleX, "appleY", appleY);
+             };
 }
 
 window.addEventListener('keydown', keyDown, false);
